7fe71c424da1577ac2e6baf149977dd325efcb55
roguestudios21 <atharv.choughule@gmail.com>
2025-07-31 15:40:51 +0530

    practice

---
+++ RockPaperScissors/finalView.swift
@@ -0,0 +1,18 @@
+//
+//  finalView.swift
+//  RockPaperScissors
+//
+//  Created by Atharv  on 28/07/25.
+//
+
+import SwiftUI
+
+struct finalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    finalView()
+}
